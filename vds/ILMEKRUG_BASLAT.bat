e3e852add1109d47c191197f74ed68d4cea2d475
Yasin Ayaz <yayaz4226@gmail.com>
2026-05-26 18:13:31 +0300

    Use ASCII IlmekRug in VDS launcher files

--- vds/ILMEKRUG_BASLAT.bat
+++ vds/ILMEKRUG_BASLAT.bat
@@ -2,7 +2,7 @@
 setlocal
 
 REM IlmekRug bilgisayari icin tek tik baslatici.
-set "STORE_ID=İlmekRug"
+set "STORE_ID=IlmekRug"
 set "GOOGLE_SHEET_ID=12zcGd3Ila-y_aZWCldNZUeJp-1yBrz_Uvh4Yf3U0f7o"
 set "GOOGLE_CREDS_JSON=C:\rugskilim-panel\streamlit\credentials.json"
 set "TEMP_DIR=C:\etsy_temp\IlmekRug"
